8ad3b6bf40ef59404f88436b0f016b9e70dc6c80
AlmazKh <32297268+AlmazKh@users.noreply.github.com>
2019-11-22 01:03:06 +0300

    Add rx hw

--- src/main/java/ru/vlados/rxhomework/hw1.kt
+++ src/main/java/ru/vlados/rxhomework/hw1.kt
@@ -1,34 +1,55 @@
 @file:JvmName("Main")
+
 package ru.vlados.rxhomework
 
 import io.reactivex.Flowable
 import ru.vlados.rxhomework.internal.Permission
+import ru.vlados.rxhomework.internal.getUsersFromSource1
+import ru.vlados.rxhomework.internal.getUsersFromSource2
+import java.util.concurrent.TimeUnit
 
 // flatMap users to permissions,
 // distinct their
 fun task1(): Flowable<Permission> {
     // return getUsersFromSource1()...
-    TODO()
+    return getUsersFromSource1()
+            .flatMapIterable {
+                it.permissions
+            }
+            .distinct()
 }
 
 // flatMap users to permissions with random delay,
 // distinct their
 fun task2(): Flowable<Permission> {
     // return getUsersFromSource2()...
-    TODO()
+    return getUsersFromSource2()
+            .flatMap {
+                Flowable.fromIterable(it.permissions)
+                        .delay((1000..4000).random().toLong(), TimeUnit.MILLISECONDS)
+            }
+            .distinct()
 }
 
 // concatMap users to permissions with random delay,
 // distinct their
 fun task3(): Flowable<Permission> {
     // return getUsersFromSource2()...
-    TODO()
+    return getUsersFromSource2()
+            .delay((1000..4000).random().toLong(), TimeUnit.MILLISECONDS)
+            .concatMapIterable {
+                it.permissions
+            }
+            .distinct()
 }
 
 // map user from different sources to usernames,
 // merge their
 fun task4(): Flowable<String> {
-    TODO()
+    return Flowable.merge(getUsersFromSource1(), getUsersFromSource2())
+            .map {
+                it.username
+            }
 }
 
 // implement 2 times retrying
--- src/test/java/ru/vlados/rxhomework/HwTest.kt
+++ src/test/java/ru/vlados/rxhomework/HwTest.kt
@@ -1,8 +1,20 @@
 package ru.vlados.rxhomework
 
+import io.reactivex.plugins.RxJavaPlugins
+import io.reactivex.schedulers.Schedulers
+import org.junit.Before
 import org.junit.Test
+import ru.vlados.rxhomework.internal.forTest
+import ru.vlados.rxhomework.internal.forTestWithError
+
+private val SCHEDULER_INSTANCE = Schedulers.trampoline()
 
 class HwTest {
+
+    @Before
+    fun setUp() {
+        RxJavaPlugins.setIoSchedulerHandler{SCHEDULER_INSTANCE}
+    }
 
     // make sure that
     //     value at index 0 equals 5;
@@ -11,7 +23,11 @@ class HwTest {
     //     chain is completed
     @Test
     fun task6() {
-        // forTest().test()
+        forTest().test()
+                .assertValueAt(0, 5)
+                .assertValueAt(2, 1)
+                .assertComplete()
+                .assertNoErrors()
     }
 
     // make sure that
@@ -19,7 +35,14 @@ class HwTest {
     //     type of error is IllegalArgumentException
     @Test
     fun task7() {
-        // forTestWithError().test()
-    }
+        val error = IllegalArgumentException::class.java
 
+        forTestWithError().subscribeOn(SCHEDULER_INSTANCE)
+                .test()
+                .assertValueAt(0, 1)
+                .assertValueAt(1, 2)
+                .assertValueAt(2, 3)
+                .assertValueAt(3, 4)
+                .assertError(error)
+    }
 }
