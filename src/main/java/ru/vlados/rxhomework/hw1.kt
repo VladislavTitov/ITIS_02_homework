41c63f9a0dba39a4c8f45eff76f135308b63acbc
kehtolaulu <emilia270627@gmail.com>
2019-11-22 22:55:28 +0300

    wip

--- src/main/java/ru/vlados/rxhomework/hw1.kt
+++ src/main/java/ru/vlados/rxhomework/hw1.kt
@@ -1,34 +1,50 @@
 @file:JvmName("Main")
+
 package ru.vlados.rxhomework
 
 import io.reactivex.Flowable
-import ru.vlados.rxhomework.internal.Permission
+import io.reactivex.Flowable.just
+import ru.vlados.rxhomework.internal.*
+import java.util.concurrent.TimeUnit
+
+object Random {
+    val random = java.util.Random()
+    fun next(): Long {
+        return random.nextLong() % 1000
+    }
+}
 
 // flatMap users to permissions,
-// distinct their
+// distinct them
 fun task1(): Flowable<Permission> {
-    // return getUsersFromSource1()...
-    TODO()
+    return getUsersFromSource1().flatMap {
+        Flowable.fromIterable(it.permissions)
+    }.distinct()
 }
 
 // flatMap users to permissions with random delay,
-// distinct their
+// distinct them
 fun task2(): Flowable<Permission> {
-    // return getUsersFromSource2()...
-    TODO()
+    return getUsersFromSource2().flatMap {
+        Flowable.fromIterable(it.permissions)
+    }.distinct().delay(Random.next(), TimeUnit.MILLISECONDS)
 }
 
 // concatMap users to permissions with random delay,
-// distinct their
+// distinct them
 fun task3(): Flowable<Permission> {
-    // return getUsersFromSource2()...
-    TODO()
+    return getUsersFromSource2().concatMap {
+        Flowable.fromIterable(it.permissions)
+    }.distinct().delay(Random.next(), TimeUnit.MILLISECONDS)
 }
 
 // map user from different sources to usernames,
-// merge their
+// merge them
 fun task4(): Flowable<String> {
-    TODO()
+    return Flowable.merge(
+        getUsersFromSource1(),
+        getUsersFromSource2()
+    ).map(User::username)
 }
 
 // implement 2 times retrying
@@ -36,27 +52,36 @@ fun task4(): Flowable<String> {
 // step equals 2 sec;
 // on 3rd time throw error
 fun task5(): Flowable<Int> {
-    // return withError()...
-    TODO()
+    return withError().retryWhen { errors ->
+        errors.zipWith(1..2) { e, i -> e to i }
+            .flatMap { just(it).delay(2 * it.second.toLong(), TimeUnit.SECONDS) }
+            .map {
+                when (it.second) {
+                    2 -> throw it.first
+                    else -> it
+                }
+            }
+    }
 }
 
 fun main() {
-    task1().blockingForEach { println(it) }
-    println()
+//    task1().blockingForEach { println(it) }
+//    println()
+//
+//    task2().blockingForEach { println(it) }
+//    println()
+//
+//    task3().blockingForEach { println(it) }
+//    println()
+//
+//    task4().blockingForEach { println(it) }
+//    println()
+//
+//    task3().blockingForEach { println(it) }
+//    println()
+//
+//    task4().blockingForEach { println(it) }
+//    println()
 
-    task2().blockingForEach { println(it) }
-    println()
-
-    task3().blockingForEach { println(it) }
-    println()
-
-    task4().blockingForEach { println(it) }
-    println()
-
-    task5().blockingSubscribe({
-        println(it)
-    }, {
-        println(it)
-    })
-    println()
+    task5().blockingSubscribe(::println, ::println)
 }
--- src/test/java/ru/vlados/rxhomework/HwTest.kt
+++ src/test/java/ru/vlados/rxhomework/HwTest.kt
@@ -1,6 +1,12 @@
 package ru.vlados.rxhomework
 
+import io.reactivex.Flowable
+import io.reactivex.plugins.RxJavaPlugins
+import io.reactivex.schedulers.Schedulers
 import org.junit.Test
+import ru.vlados.rxhomework.internal.forTest
+import ru.vlados.rxhomework.internal.forTestWithError
+import java.lang.IllegalArgumentException
 
 class HwTest {
 
@@ -11,7 +17,16 @@ class HwTest {
     //     chain is completed
     @Test
     fun task6() {
-        // forTest().test()
+        RxJavaPlugins.setIoSchedulerHandler {
+            Schedulers.trampoline()
+        }
+
+        val testSubscriber = forTest().test()
+        testSubscriber.assertValueAt(0, 5)
+        testSubscriber.assertValueAt(2, 1)
+        testSubscriber.assertNoErrors()
+        testSubscriber.assertComplete()
+
     }
 
     // make sure that
@@ -19,7 +34,12 @@ class HwTest {
     //     type of error is IllegalArgumentException
     @Test
     fun task7() {
-        // forTestWithError().test()
+        RxJavaPlugins.setIoSchedulerHandler {
+            Schedulers.trampoline()
+        }
+        val testSubscriber = forTestWithError().test()
+        testSubscriber.assertValues(1, 2, 3, 4)
+        testSubscriber.assertError(IllegalArgumentException::class.java)
     }
 
 }
