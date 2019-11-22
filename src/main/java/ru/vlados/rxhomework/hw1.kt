441254afe31600f7bce32db77e027d11471fe622
dkiryanov <dkiryanov@fix.ru>
2019-11-22 09:48:06 +0300

    homework

--- src/main/java/ru/vlados/rxhomework/hw1.kt
+++ src/main/java/ru/vlados/rxhomework/hw1.kt
@@ -1,44 +1,60 @@
 @file:JvmName("Main")
+
 package ru.vlados.rxhomework
 
 import io.reactivex.Flowable
+import io.reactivex.functions.BiFunction
 import ru.vlados.rxhomework.internal.Permission
+import ru.vlados.rxhomework.internal.getUsersFromSource1
+import ru.vlados.rxhomework.internal.getUsersFromSource2
+import ru.vlados.rxhomework.internal.withError
+import java.util.concurrent.TimeUnit
 
 // flatMap users to permissions,
 // distinct their
-fun task1(): Flowable<Permission> {
-    // return getUsersFromSource1()...
-    TODO()
-}
+fun task1(): Flowable<Permission> =
+    getUsersFromSource1()
+        .flatMapIterable { it.permissions }
+        .distinct()
 
 // flatMap users to permissions with random delay,
 // distinct their
-fun task2(): Flowable<Permission> {
-    // return getUsersFromSource2()...
-    TODO()
-}
+fun task2(): Flowable<Permission> =
+    getUsersFromSource2()
+        .flatMapIterable { it.permissions }
+        .delay((1..5).random().toLong(), TimeUnit.SECONDS)
+        .distinct()
 
 // concatMap users to permissions with random delay,
 // distinct their
-fun task3(): Flowable<Permission> {
-    // return getUsersFromSource2()...
-    TODO()
-}
+fun task3(): Flowable<Permission> =
+    getUsersFromSource2()
+        .concatMapIterable { it.permissions }
+        .delay((1..5).random().toLong(), TimeUnit.SECONDS)
+        .distinct()
 
 // map user from different sources to usernames,
 // merge their
-fun task4(): Flowable<String> {
-    TODO()
-}
+fun task4(): Flowable<String> =
+    Flowable.merge(getUsersFromSource1(), getUsersFromSource2())
+        .map { it.username }
 
 // implement 2 times retrying
 // with linear-grown delay,
 // step equals 2 sec;
 // on 3rd time throw error
-fun task5(): Flowable<Int> {
-    // return withError()...
-    TODO()
-}
+fun task5(): Flowable<Int> =
+    withError().retryWhen {
+        Flowable.zip<Throwable, Int, Int>(it, Flowable.range(1, 3),
+            BiFunction { error, count ->
+                if (count == 3) throw error
+                else count
+            })
+            .flatMap { count ->
+                val delayTime = 2L * count
+                Flowable.just(it).delay(delayTime, TimeUnit.SECONDS)
+            }
+    }
 
 fun main() {
     task1().blockingForEach { println(it) }
--- src/test/java/ru/vlados/rxhomework/HwTest.kt
+++ src/test/java/ru/vlados/rxhomework/HwTest.kt
@@ -1,8 +1,20 @@
 package ru.vlados.rxhomework
 
+import io.reactivex.plugins.RxJavaPlugins
+import io.reactivex.schedulers.Schedulers
+import org.junit.Before
 import org.junit.Test
+import ru.vlados.rxhomework.internal.forTest
+import ru.vlados.rxhomework.internal.forTestWithError
 
 class HwTest {
+
+    @Before
+    fun init() {
+        RxJavaPlugins.setIoSchedulerHandler {
+            Schedulers.trampoline()
+        }
+    }
 
     // make sure that
     //     value at index 0 equals 5;
@@ -11,7 +23,12 @@ class HwTest {
     //     chain is completed
     @Test
     fun task6() {
-        // forTest().test()
+        forTest()
+            .test()
+            .assertValueAt(0, 5)
+            .assertValueAt(2, 1)
+            .assertComplete()
+            .assertNoErrors()
     }
 
     // make sure that
@@ -19,7 +36,12 @@ class HwTest {
     //     type of error is IllegalArgumentException
     @Test
     fun task7() {
-        // forTestWithError().test()
+        forTestWithError()
+            .test()
+            .assertValueAt(0, 1)
+            .assertValueAt(1, 2)
+            .assertValueAt(2, 3)
+            .assertValueAt(3, 4)
+            .assertError(IllegalArgumentException::class.java)
     }
-
 }
