8ceb2e2b24f3b200b884bf39dfbc99ca6aed1043
iShamSLam <31825566+iShamSLam@users.noreply.github.com>
2019-11-22 20:01:10 +0300

    RxHomeDonWork

--- src/test/java/ru/vlados/rxhomework/HwTest.kt
+++ src/test/java/ru/vlados/rxhomework/HwTest.kt
@@ -1,25 +1,45 @@
 package ru.vlados.rxhomework
 
+import io.reactivex.plugins.RxJavaPlugins
+import io.reactivex.schedulers.Schedulers
+import org.junit.Before
 import org.junit.Test
+import ru.vlados.rxhomework.internal.forTest
+import ru.vlados.rxhomework.internal.forTestWithError
+
 
 class HwTest {
+    private val schedulerInstance = Schedulers.trampoline()
+
+    @Before
+    fun setUp() {
+        RxJavaPlugins.setIoSchedulerHandler { schedulerInstance }
+    }
 
     // make sure that
-    //     value at index 0 equals 5;
-    //     value at index 2 equals 1;
-    //     errors are not present;
-    //     chain is completed
+    // value at index 0 equals 5;
+    // value at index 2 equals 1;
+    // errors are not present;
+    // chain is completed
     @Test
     fun task6() {
-        // forTest().test()
+        forTest().test()
+            .assertValueAt(0, 5)
+            .assertValueAt(2, 1)
+            .assertComplete()
+            .assertNoErrors()
     }
 
     // make sure that
-    //     values are the same with [1, 2, 3, 4];
-    //     type of error is IllegalArgumentException
+    // values are the same with [1, 2, 3, 4];
+    // type of error is IllegalArgumentException
     @Test
     fun task7() {
-        // forTestWithError().test()
-    }
+        val error = IllegalArgumentException::class.java
 
+        forTestWithError().subscribeOn(schedulerInstance)
+            .test()
+            .assertValues(1, 2, 3, 4)
+            .assertError(error)
+    }
 }
